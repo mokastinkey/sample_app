75f12278b8c20564b44c3cb71257ef36b200d17d
mokastinkey <mocastinkey@gmail.com>
2021-07-01 10:18:17 +0000

    [finish]sample_app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '5cd7dccdaad726ad66c3d57bfccb91834af485c9e2682a2b68cbed9f7583ebdc5ac6458fade80e920107d95bac1a038207e7a4e15f77e748bc3d48b5648130de'
